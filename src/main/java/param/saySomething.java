de821d299afde8d0e946fea9925e53b3209a5914
ywhnaxierizi <1549766972@qq.com>
2018-11-18 10:52:14 +0800

    test

---
+++ src/main/java/param/saySomething.java
@@ -0,0 +1,14 @@
+package param;
+
+import java.io.PrintStream;
+
+public class saySomething {
+	public static PrintStream o;
+	
+	public void doSome(String str) {
+		this.o.print("str");
+	}
+	
+	
+
+}
---
+++ src/main/java/test/test20181118.java
@@ -0,0 +1,13 @@
+package test;
+
+import param.saySomething;
+
+public class test20181118 {
+	test20181118(){
+		
+	}
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+}
